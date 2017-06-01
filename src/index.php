02c5d25926f2a5cc2f126cfa1fe09a494903c3df
Mladen Plavsic <mladen.plavsic@gmail.com>
2017-06-01 10:47:19 +0200

    Menu adjustments

--- src/index.php
+++ src/index.php
@@ -102,19 +102,17 @@
                 </li>
             </ul>
             <div ng-if="::item.children" ng-show="open" class="panel-body" ng-init="$parent.tab=1">
-                <div ng-show="$parent.tab===1" class="row">
-                    <div class="col-xs-6" ng-repeat="child in ::item.children">
-                        <div class="checkbox">
-                            <label>
-                                <input ng-attr-type="{{ child.type !== 'FRUSKAC_TYPE_TRACK' ? 'checkbox' : 'radio' }}"
-                                       ng-click="item.getVisible()&&(child.type !== 'FRUSKAC_TYPE_TRACK'?child.setVisible(!child.getVisible()):child.focus())"
-                                       ng-checked="child.getVisible()"
-                                       ng-disabled="!item.getVisible()"
-                                       ng-class="'color-'+item.id+'-'+child.id"
-                                >
-                                {{ ::child.id | translate:'child' }}
-                            </label>
-                        </div>
+                <div ng-show="$parent.tab===1" class="column-list">
+                    <div ng-repeat="child in ::item.children" class="checkbox">
+                        <label>
+                            <input ng-attr-type="{{ child.type !== 'FRUSKAC_TYPE_TRACK' ? 'checkbox' : 'radio' }}"
+                                   ng-click="item.getVisible()&&(child.type !== 'FRUSKAC_TYPE_TRACK'?child.setVisible(!child.getVisible()):child.focus())"
+                                   ng-checked="child.getVisible()"
+                                   ng-disabled="!item.getVisible()"
+                                   ng-class="'color-'+item.id+'-'+child.id"
+                            >
+                            {{ ::child.id | translate:'child' }}
+                        </label>
                     </div>
                 </div>
                 <div ng-show="$parent.tab===2" class="row">
